4888a7c66437e90aca6d79463b6498a7eacd2b69
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-11-22 14:56:40 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import Title from './components/Title.vue';
+export default {
+  components: {
+    Title,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<template>
+  <Title />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style>
+</style>
